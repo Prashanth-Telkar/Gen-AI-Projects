8b8fdee8e57b4149b67f9310fea87aa7cacd0333
Prashanth-Telkar <prashanthtelkar@gmail.com>
2024-07-28 12:43:12 +0530

    Q&A chatbot

---
+++ ChatBot/app.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import openai
+import langchain
+#from langchain_openai import ChatOpenAI
+from langchain_community.llms import Ollama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+#langsmith Tracking
+os.environ['LANGCHAIN_API_KEY'] = os.getenv("LANGCHAIN_API_KEY")
+os.environ['LANGCHAIN_TRACING_V2'] = "true"
+os.environ['LANGCHAIN_PROJECT'] = "Q&A Chat-Bot with OpenAI"
+
+# Prompt templates
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant, Please respond to the user queries"),
+        ("user","Question:{question}")
+    ]
+)
+
+def generate_response(question,api_key,llm,temperature,max_tokens):
+    #openai.api_key=api_key
+    #llm = ChatOpenAI(model=llm)
+    llm = Ollama(model=llm)
+    outputparser=StrOutputParser()
+    chain=prompt|llm|outputparser
+    answer = chain.invoke({"question":question})
+    return answer
+
+# Title of the app
+st.title("Enhanced ChatBot")
+
+# Side bar for settings
+st.sidebar.title("Settings")
+api_key = st.sidebar.text_input("Enter your API Key:",type="password")
+
+# Drop down to select different models
+llm = st.sidebar.selectbox("Select the model",['gpt-4o','gpt-4-turbo','gpt-4',"phi3"])
+
+# Adjust response parameter
+temperature = st.sidebar.slider("Temperature",min_value=0.0,max_value=1.0,value=0.7)
+max_tokens = st.sidebar.slider("Max tokens",min_value=50,max_value=300,value=150)
+
+# main interface for user input
+st.write("Go ahead and ask any question")
+user_input = st.text_input("You:")
+
+if user_input:
+    response = generate_response(user_input,api_key=api_key,llm=llm,temperature=temperature,max_tokens=max_tokens)
+    st.write(response)
+else:
+    st.write("Please ask the question")
+
+
+
+
+
